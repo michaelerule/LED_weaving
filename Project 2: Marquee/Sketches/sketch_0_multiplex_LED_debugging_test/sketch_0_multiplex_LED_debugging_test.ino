153eff60889339d9120919de842ddf05fae28db9
M Rule <mrule7404@gmail.com>
2019-02-24 15:48:45 +0000

    add some photos; working on sketchs for project 2; code for uploading bitmap fonts on Arduino
    Former-commit-id: ab7e8ae6a0c3922a8c0b90b82aeb0b46df662063
    Former-commit-id: 53590878e2a2e3dfc808c7adf387cf9e9cdc4719 [formerly 37307de862f7cfe683483d55e8458574fe981c60] [formerly ef026a0a6b5c55375d7b60ea542e32d611c7db50 [formerly cdd6723a16975377ecd6a3eb9c28e03fa1173549]]
    Former-commit-id: 4e97973de9875cd0cbaac6d8ab5fee000b1408d1 [formerly 6854c88797ac89e962c21c612f8e4fc4ea67e831]
    Former-commit-id: e13150f87398bc4146c9a577af1021da47048f60
    Former-commit-id: 22460bb8b740203ae129f0639187af8c2aea1710

---
+++ Project 2: Marquee/Sketches/sketch_0_multiplex_LED_debugging_test/sketch_0_multiplex_LED_debugging_test.ino
@@ -0,0 +1,163 @@
+/*  Debugging script for charlieplexing LED configuration.
+
+    Use this script to test turning all lights on/off, and to set or determine 
+    the mapping between Arduino pin numbers and control lines for the LED 
+    display. 
+
+    This is designed for the AtMega168/328-based boards and will not work on 
+    other Arduino platforms, since it uses port mappings to more efficiently 
+    control the LEDs.
+
+    This implementation uses a timer interrupt to scane the LEDs one at a time.
+
+    DDRx  : 1 = output, 0 = input
+    PORTx : output buffer
+    PINx  : digital input buffer ( writes set pullups )
+
+    Arduino Uno Pin mapping
+                          __ __
+         !RESET     PC6 -|  U  |- PC5    19 (a5)
+         0 (rx)     PD0 -|     |- PC4    18 (a4)
+         1 (tx)     PD1 -|     |- PC3    17 (a3)
+         2          PD2 -|     |- PC2    16 (a2)
+         3 (pwm)    PD3 -|  m  |- PC1    15 (a1)
+         4          PD4 -|  3  |- PC0    14 (a0)
+                    VCC -|  2  |- GND
+                    GND -|  8  |- AREF
+         xtal       PB6 -|     |- AVCC
+         xtal       PB7 -|     |- PB5    13 SCK  ( yello )
+         5 (pwm)    PD5 -|     |- PB4    12 MISO ( green )
+         6 (pwm)    PD6 -|     |- PB3    11 MOSI ( blue )
+         7          PD7 -|     |- PB2    10
+         8          PB0 -|_____|- PB1     9
+
+        Programmer pinout, 6 pin:
+                
+        6 MISO +-+  VCC 3
+        5 SCK  + + MOSI 2 
+        4 RST  +-+  GND 1
+
+        Programmer pinout, 10 pin:
+                
+        3 vcc  +-+   MOSI 2
+               + +    
+               + +]  RST  4 
+               + +   SCK  5 
+        1 gnd  +-+   MISO 6
+*/
+
+// Include support for timer interrupts
+#include <avr/interrupt.h>
+
+// delay between turning lghts on/off in ms
+#define DELAY 5
+
+// We can control NLINES*(NLINES-1) LEDs using NLINES pins. For now, we'll
+// define the grid in terms of the charlielexing layout between pins, rather 
+// than the actual physical arrangement of LEDs. NLINES should be odd.
+#define NLINES 11
+#define NROWS (NLINES+1)
+#define NCOLS (NLINES+1)
+
+// Map from LED grid line number to arduino pin
+const uint8_t lineToPin[NLINES] {11,12,2,4,6,8,10,9,7,5,3,};
+
+// Volatile state variables for timer interrupt routine
+volatile uint16_t display_data[NLINES];
+volatile int      line1 = 0;
+volatile int      line2 = 0;
+
+/** Timer ineterrupt to scan the display
+ */
+SIGNAL(TIMER2_COMPA_vect) 
+{
+  // everything to INPUT mode
+  DDRB  = DDRC  = DDRD  = 0;
+
+  // Increment loop counters
+  do if (++line2>=NLINES) {
+      line2=0;
+      line1=(line1+1)%NLINES;
+  } while (line1==line2);
+
+  // Show ONE pin
+  if ((display_data[line1]>>line2)&1) {
+    int p1 = lineToPin[line1];
+    int p2 = lineToPin[line2];
+    digitalWrite(p1,1);
+    digitalWrite(p2,0);
+    pinMode(p1,1);
+    pinMode(p2,1);
+  }
+}
+
+void setup() {
+  // Set everything LOW and INPUT mode
+  DDRB  = DDRC  = DDRD  = 0;
+  PORTB = PORTC = PORTD = 0;
+  
+  // Zero display buffer
+  for (int line=0; line<NLINES; line++)
+    display_data[line]=0;
+
+  // Arduino environment might be already running interrupts
+  // Temporarily disable interrupts as we configure
+  cli();
+  // Can't use timer 0 since arduino uses it!!
+  // Use the TIMER2_OVF_vect
+  // Set clock divisor (small 3 bits of TCCR2B)
+  // Possible values are (0 through 7): 1 8 32 64 128 256 1024
+  TCCR2B=(TCCR2B&0b11111000)|0b001;
+  // Set the output compare A bit for timer 2
+  TIMSK2 |= 0b010;
+  // Set timer period
+  OCR2A = 1;
+  // Allow interrupts
+  sei();
+}
+
+/* Convert between display coordinates and LED grid 
+ * coordinates.
+ */
+inline void displayToGrid(int row, int col, int *i, int *j) {
+  //row = (row-col+NROWS)%NROWS;
+  //col = (col-row+NCOLS)%NCOLS;
+  if (row<col) row++;
+  *i = col;//lineToPin[col];
+  *j = row;//lineToPin[row];
+}
+
+/* Set given pixel to "on"
+ */
+void set_pixel(int row, int col) {
+  int i,j;
+  displayToGrid(row,col,&i,&j);
+  display_data[i]|=1<<j;
+}
+
+/* Set given pixel to "off"
+ */
+void clear_pixel(int row, int col) {
+  int i,j;
+  displayToGrid(row,col,&i,&j);
+  display_data[i]&=~(1<<j);
+}
+
+void loop() {
+  for (int col=0; col<NCOLS; col++) {
+    for (int row=0; row<NROWS; row++) {
+      set_pixel(row,col);
+      delay(DELAY);
+    }
+  }
+  for (int col=0; col<NCOLS; col++) {
+    for (int row=0; row<NROWS; row++) {
+      clear_pixel(row,col); 
+      delay(DELAY);
+    }
+  }
+  
+}
+
+
+ 
